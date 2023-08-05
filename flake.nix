153276f49c4b3aec819c99f92f9a53d6e659afca
specCon18 <steven.carpenter@skdevstudios.com>
2023-08-04 23:11:42 -0400

    fixed flake shell commands

--- flake.nix
+++ flake.nix
@@ -12,12 +12,12 @@
       #!/usr/bin/env zsh
       set -e
       export PATH=${pkgs.nodejs_20}/bin:${pkgs.nodePackages_latest.pnpm}/bin:$PATH
-      pnpm dlx tailwindcss -i styles/tailwind.css -o assets/main.css --watch
+      pnpm dlx tailwindcss -i src/styles/tailwind.css -o assets/main.css --watch
     '';
     run-dev = pkgs.writeShellScriptBin "run-dev" ''
       #!/usr/bin/env zsh
       set -e
-      cargo run test.json
+      cargo run test_data/test.json
     '';
 
   in {
@@ -30,7 +30,7 @@
         pkgconfig
         rustc
         cargo
-        nodejs-20
+        nodejs_20
         nodePackages_latest.pnpm
       ];
 
